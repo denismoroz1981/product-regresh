77326d1fef569efcc9527c283196b307ae560d3e
denismoroz1981 <denismoroz@mail.ru>
2018-07-12 20:43:41 +0300

    migrattion for Offers created

--- console/migrations/m180516_185527_init_offers_table.php
+++ console/migrations/m180516_185527_init_offers_table.php
@@ -14,8 +14,11 @@ class m180516_185527_init_offers_table extends Migration
     {
          $this->createTable('offers',
              [
+
+
                  'admin_id'=>'pk',
                  'street_name'=>'string',
+                 'price_item'=>'integer',
                  'rooms_count'=>'integer',
                  'type'=>'string',
                  'is_commercial'=>'integer',
@@ -26,13 +29,15 @@ class m180516_185527_init_offers_table extends Migration
                  'metro_station_name'=>'string',
                  'wall_type'=>'string',
                  'publishing_date'=>'string',
-                 'price'=>'integer',
+                 //'price'=>'integer',
                  'realty_type_name'=>'string',
+                 'realty_sale_type'=>'integer',
                  'latitude'=>'string',
+                 'longitude'=>'string',
                  'main_photo'=>'string',
                  'building_number_str'=>'string',
                  'city_name'=>'string',
-                 'living_square_meters'=>$this->float(),
+                 'living_square_meters'=>'integer',
                  'realty_type_id'=>'integer',
                  'floors_count'=>'integer',
                  'kitchen_square_meters'=>'integer',
@@ -43,6 +48,27 @@ class m180516_185527_init_offers_table extends Migration
                  'district_name'=>'string',
                  'advert_type_name'=>'string',
                  'advert_type_id'=>'integer',
+                 'price_type'=>'string',
+                 'created_at'=>'string',
+                 'levels_expired'=>'string',
+                 'is_exchange'=>'string',
+                 'floor'=>'integer',
+                 'is_bargain'=>'integer',
+                 'user'=>'string',
+                 'priceUSD'=>'integer',//add entering
+                 'c1501'=>'integer', //кухня-студия
+                 'c1502'=>'integer', //многоуровневая
+                 'c1503'=>'integer', //с мансардой
+                 'c1504'=>'integer', //пентхаус
+                 'c443'=>'integer', //год постройки
+                 'c1607'=>'integer', //тариф ЖКХ зима
+                 'c1608'=>'integer', //тариф ЖКХ лето
+                 'c1011'=>'integer', //цена договорная
+                 'c1464'=>'integer', //цена стартовая
+                 'c274'=>'integer', //возможна рассрочка/кредит
+                 'c265'=>'integer', //возможен обмен
+                 'c1437'=>'integer', //тип предложения
+
 
                  //'admin_time_entered'=>$this->dateTime().' DEFAULT NOW()',
                  'admin_time_entered'=>'TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP'
